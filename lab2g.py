4c405988672cbaae719db885a2489591cd71001c
Ajit Virk <avirk18@myseneca.ca>
2024-09-25 23:47:47 -0400

    Lab2g Script

---
+++ lab2g.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+    timer = 3
+
+else:
+    timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
